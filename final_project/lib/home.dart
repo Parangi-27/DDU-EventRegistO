2980e4696a1596870598a5620c28a541b55860fe
Parangi Rathod <82773925+Parangi-27@users.noreply.github.com>
2022-10-19 10:12:52 +0530

    home page added

--- final_project/lib/home.dart
+++ final_project/lib/home.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-
+import 'package:final_project/navbar.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: MyHome(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
 class MyHome extends StatefulWidget {
   const MyHome({Key? key}) : super(key: key);
 
@@ -8,11 +24,104 @@ class MyHome extends StatefulWidget {
   State<MyHome> createState() => _MyHomeState();
 }
 
+final List<String> urlImages = [
+  'https://unsplash.com/photos/MRjjcDIk3Gw',
+  'https://unsplash.com/photos/MRjjcDIk3Gw',
+  'https://unsplash.com/photos/MRjjcDIk3Gw',
+];
+
 class _MyHomeState extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Text("Home"),
+    return Scaffold(
+        drawer: NavBar(),
+        appBar: AppBar(
+          title: Text('DDU Evento'),
+          backgroundColor: Colors.deepOrangeAccent,
+        ),
+      body: ListView(
+        children: [
+          SizedBox(height: 35),
+          CarouselSlider(
+            items: [
+              //1st Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: NetworkImage('https://picsum.photos/250?image=9'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              //2nd Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: NetworkImage('https://picsum.photos/250?image=9'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              //3rd Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: NetworkImage('https://picsum.photos/250?image=9'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              //4th Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: NetworkImage('https://picsum.photos/250?image=9'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              //5th Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: NetworkImage('https://picsum.photos/250?image=9'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+            ],
+
+            //Slider Container properties
+            options: CarouselOptions(
+              height: 180.0,
+              enlargeCenterPage: true,
+              autoPlay: true,
+              aspectRatio: 16 / 9,
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enableInfiniteScroll: true,
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              viewportFraction: 0.8,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
+
---
+++ final_project/lib/navbar.dart
@@ -0,0 +1,86 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+                accountName: Text('Dharmsinh Desai University'),
+                accountEmail: Text('Nadiad'),
+                currentAccountPicture: CircleAvatar(
+                  child: ClipOval(
+                    child: Image.asset(
+                      'assets/ddu.jpg',
+                      width: 90,
+                      height: 90,
+                      fit: BoxFit.cover
+                    ),
+                  ),
+                ),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/ddu2.jpg'
+                  ),
+                  fit: BoxFit.cover,
+                )
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.event),
+              title: Text('Upcoming Events'),
+              onTap: () => null,
+              trailing: ClipOval(
+              child: Container(
+                color: Colors.red,
+                width: 20,
+                height: 20,
+
+                child:Center(
+                child: Text(
+                  '2',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+
+                  ),
+                ),
+              ),
+              ),
+            ),
+            ),
+            ListTile(
+              leading: Icon(Icons.access_time_rounded),
+              title: Text('Recent Events'),
+              onTap: () => null,
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text('Clubs'),
+              onTap: () => null,
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.find_replace_outlined),
+              title: Text('Resources'),
+              onTap: () => null,
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text('About Us'),
+              onTap: () => null,
+            ),
+            Divider(),
+          ],
+        ),
+    );
+  }
+}
